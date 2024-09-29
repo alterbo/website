619d93e5705e257b818b251a39dcb81083a90353
Alberto Ferrero <albertonomo@gmail.com>
2024-09-29 19:02:51 +0200

    added hover border bottom on basic buttons

--- src/js/global.js
+++ src/js/global.js
@@ -2,4 +2,4 @@ globalThis.params = new URLSearchParams(window.location.search);
 globalThis.category = params.get('category');
 globalThis.age = params.get('kids') === 'kids' ? 'kids' : 'items';
 globalThis.locale= params.get('locale');
-globalThis.hasParams = params.has('locale') && params.has('kids') && category;
+globalThis.hasParams = params.has('locale') && params.has('kids') && params.has('category');
