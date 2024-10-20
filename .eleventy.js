8da821ac4825ab109eaffd11acb3c11a9f814990
Alberto Ferrero <albertonomo@gmail.com>
2024-10-20 20:34:22 +0200

    Messing up and down listening reggeaton

--- .eleventy.js
+++ .eleventy.js
@@ -59,7 +59,6 @@ module.exports = function(eleventyConfig) {
         }
         });
     eleventyConfig.addPassthroughCopy('./src/assets/images/');
-    eleventyConfig.addPassthroughCopy('./src/js');
     return {
         dir: {
             input: 'src',
